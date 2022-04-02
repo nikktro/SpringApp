d479cf2b879e07a1b1270338cdbe42ae8507182a
Nikolay Trofimov <nikktro@gmail.com>
2022-04-03 01:36:06 +0300

    Initial Commit

---
+++ SpringApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringApp
+//
+//  Created by Nikolay Trofimov on 03.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
